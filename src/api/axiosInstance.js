e02a36fc6c1b84a3660b4026b578f38f76b976b6
ashavan33 <105936390+ashavan33@users.noreply.github.com>
2024-02-16 10:21:32 -0500

    changed base url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1'
+  baseURL: 'https://your-api-on-render.com/api/v1'
 });
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
